6be8b4d0a15898138df5e5e812e491c576cb54e3
pedrohassen <pedro_hasse@yahoo.com.br>
2023-04-09 11:40:34 -0300

    Add UserRoute which creates /users route

---
+++ backend/src/api/routes/user.route.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import type UsersController from "../controllers/users.controller";
+
+export default class UserRoute {
+  public _router: Router;
+
+  constructor(private readonly controller: UsersController) {
+    this._router = Router();
+  }
+
+  public _routes = (route = '/users'): void =>{
+    this._router.post(route, this.controller.createUser);
+  }
+}
